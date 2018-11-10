6ff6a78f7da34039e87f83f17974783e77aea66b
Tung Nguyen <thanhtuanpl1995@gmail.com>
2018-11-10 15:42:05 +0700

    xong-phan-booking

---
+++ booking-demo/src/app/models/dateTime.ts
@@ -0,0 +1,6 @@
+export class DateTime{
+    label: string;
+    day: string;
+    date:Date;
+    id: number;
+}
---
+++ booking-demo/src/app/models/stylistApi.ts
@@ -0,0 +1,9 @@
+
+import { Stylist } from './stylist';
+
+export class StylistApi{
+    success: string;
+    message: string;
+    status_code: string;
+    data: Stylist[];
+}

2ea051fe62f875bd5d5d796e07053cf1bf2ef6fd
Tung Nguyen <thanhtuanpl1995@gmail.com>
2018-11-13 17:11:00 +0700

    delete

--- booking-demo/src/app/models/shift.ts
+++
@@ -1,8 +0,0 @@
-export class Shift{
-    id: number;
-    stylist_id: number;
-    date: string;
-    start_time: string;
-    end_time: string;
-    status: string
-}
--- booking-demo/src/app/models/stylistApi.ts
+++
@@ -1,9 +0,0 @@
-
-import { Stylist } from './stylist';
-
-export class StylistApi{
-    success: string;
-    message: string;
-    status_code: string;
-    data: Stylist[];
-}

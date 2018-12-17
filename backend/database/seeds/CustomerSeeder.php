1766e0288cc4281ed50e29cbc8bbad7c372b1051
hiepdc <khonggilakhongthe26@gmail.com>
2018-12-18 03:26:51 +0700

    fix random booking

--- backend/database/seeds/CustomerSeeder.php
+++ backend/database/seeds/CustomerSeeder.php
@@ -15,29 +15,29 @@ class CustomerSeeder extends Seeder
         DB::table('customers')->insert([
             'customer_name' => "Công Hiệp",
             'phone_number' => "0976420018",
-            'coin' => "50"
+            'coin' => "200"
         ]);
         foreach (range(1, 50) as $index) {
             DB::table('customers')->insert([
                 [
                 'customer_name' => $faker->name,
                 'phone_number' => "0976".rand(123456,999999),
-                'coin' => 10*random_int(0,10),
+                'coin' => 10*random_int(1,30),
             ],
                 [
                     'customer_name' => $faker->name,
                     'phone_number' => "0126".rand(123456,999999),
-                    'coin' => 10*random_int(0,10),
+                    'coin' => 10*random_int(1,40),
                 ],
                 [
                     'customer_name' => $faker->name,
                     'phone_number' => "0369".rand(123456,999999),
-                    'coin' => 10*random_int(0,10),
+                    'coin' => 10*random_int(1,20),
                 ],
                 [
                     'customer_name' => $faker->name,
                     'phone_number' => "0905".rand(123456,999999),
-                    'coin' => 10*random_int(0,10),
+                    'coin' => 10*random_int(1,10),
                 ]
             ]);
 
